0106c03d1c919c36df3ce7887d5fd630a7372f97
gluknik <gluknik@gmail.com>
2022-10-07 20:42:36 +0300

    Adds scrapper

--- public/main.js
+++ public/main.js
@@ -35,32 +35,34 @@ var form = document.getElementById("login-form");
 //   });
 // });
 
-login.addEventListener("click", function (e) {
-  e.preventDefault();
+if (login) {
+  login.addEventListener("click", function (e) {
+    e.preventDefault();
 
-  const username = form.elements["username"];
-  const password = form.elements["password"];
+    const username = form.elements["username"];
+    const password = form.elements["password"];
 
-  fetch("/login", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      username: document.querySelector("#username").value,
-      password: document.querySelector("#password").value,
-    }),
-  })
-    .then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
+    fetch("/login", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: document.querySelector("#username").value,
+        password: document.querySelector("#password").value,
+      }),
     })
-    .then((data) => {
-      if (data) {
-        //console.log(data);
-        window.location.href = data.url;
-      }
-    })
-    .catch((err) => console.error(err));
-});
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        if (data) {
+          //console.log(data);
+          window.location.href = data.url;
+        }
+      })
+      .catch((err) => console.error(err));
+  });
+}
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
+const puppeteer = require("puppeteer");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const { env } = require("process");
 const MongoClient = require("mongodb").MongoClient;
 
 var db;
@@ -9,6 +11,8 @@ var db;
 const MONGO_USERNAME = process.env.MONGO_USERNAME;
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD;
 const MONGO_CLUSTER_URL = process.env.MONGO_CLUSTER_URL;
+const REMOTE_PC_USERNAME = process.env.REMOTE_PC_USERNAME;
+const REMOTE_PC_PASSWORD = process.env.REMOTE_PC_PASSWORD;
 
 // Remember to change YOUR_USERNAME and YOUR_PASSWORD to your username and password!
 MongoClient.connect(
@@ -26,6 +30,93 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
+
+// SCRAPPER
+
+const statuses = [];
+
+// Functions
+const addStatuses = () => {
+  db.collection("statuses").insertMany(statuses, (err, result) => {
+    if (err) return console.log(err);
+    console.log("saved to database");
+  });
+};
+
+const updateStatuses = () => {
+  statuses.forEach((status, index) => {
+    db.collection("statuses").findOneAndUpdate(
+      { _id: index },
+      {
+        $set: {
+          updated: status.updated,
+        },
+      },
+      {
+        sort: { _id: -1 },
+        upsert: true,
+      },
+      (err, result) => {
+        if (err) return err;
+        console.log(result);
+      },
+    );
+  });
+};
+
+console.log("inside scrapper");
+
+function runScrapper() {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const browser = await puppeteer.launch();
+      const page = await browser.newPage();
+      console.log(page);
+      await page.authenticate({
+        username: REMOTE_PC_USERNAME,
+        password: REMOTE_PC_PASSWORD,
+      });
+      await page.goto("https://news.ycombinator.com/");
+      let urls = await page.evaluate(() => {
+        let results = [];
+        let items = document.querySelectorAll(".athing");
+        items.forEach((item, index) => {
+          results.push({
+            url: item.getAttribute("href"),
+            text: item.innerText,
+            _id: index,
+            updated: false,
+          });
+        });
+        return results;
+      });
+      browser.close();
+      // console.log(urls);
+      urls.forEach((item) => {
+        statuses.push(item);
+      });
+      return resolve(urls);
+    } catch (e) {
+      return reject(e);
+    }
+  });
+}
+
+setTimeout(() => {
+  runScrapper()
+    .then(() => {
+      db.collection("statuses").findOne({}, (err, result) => {
+        //if (err) return console.log(err);
+
+        if (result) {
+          updateStatuses();
+        } else {
+          addStatuses();
+        }
+      });
+    })
+    .catch(console.error);
+}, 60000);
 
 // GET
 app.get("/", (req, res) => {
